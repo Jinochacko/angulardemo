5522d0d3360fb004e7f6139b72fab1a3971db82e
Jino Chacko <jinorocks08@gmail.com>
2021-08-02 19:45:40 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -86,7 +86,7 @@ config(['$routeProvider', function($routeProvider) {
 		var u_pass = 'test@123';
 		if((user == u_email) && (pass == u_pass)){
 			localStorage.setItem('is_logged_in',true);
-			//location.href = variables.rootUrl+'/cart/';
+			location.href = variables.rootUrl+'/cart/';
                         return true;
 		} else {
 			return false;
